c0eb7941ea7cac1e9da4c2d48f51ea10480895a1
Curly Marc <curlymar@ge-g6.kzn.21-school.ru>
2023-03-11 20:57:05 +0300

    print new

--- src/print.c
+++ src/print.c
@@ -3,11 +3,61 @@
 void print_playing_field(int x_racket1, int y_racket1, int x_racket2, int y_racket2, int x_ball, int y_ball,
                          int score1, int score2);
 
+int racket(int y_racket, char movement, int num);
+
+
 int main(void) {
-    print_playing_field(0, 5, 79, 20, 10, 20, 2, 13);
+
+    int score_racket_left = 0; //  cчет левого
+    int score_racket_right = 0; // счет правого
+    int y_racket_left = 12; // положение по игреку у левой ракетки 
+    int y_racket_rigth = 12; // положение по игреку у правой ракетки 
+    int x_ball = 39; // положение мяча по иксу (пока сделала чисто по середине)
+    int y_ball = 11; // положение мяча по игреку (пока сделала чисто по середине)
+
+    char movement_left, movement_rigth; // команды для передвижения ракеток 
+
+    
+    // сначала просто выводим поле чтоб понимать че куда
+    print_playing_field(0, y_racket_left, 79, y_racket_rigth, x_ball, y_ball, score_racket_left, score_racket_right);
+    
+    // пока никто не победил то продолжаем игру писать команды двигать мяч итд 
+    while (score_racket_left != 21 || score_racket_right != 21) {
+
+        // считываем чисто все наши команды
+        while(1) {
+            scanf("%c", &movement_left);
+            movement_rigth = movement_left;
+            if (movement_left == 'z' || movement_left == 'a' || movement_left == ' ' || movement_left == 'k' || movement_left == 'm')
+                    break; // без этой проверки не работает с пробелом
+        }
+
+        movement_rigth = movement_left;
+        if (racket(y_racket_left, movement_left, 1) == 1) { // если мы не вышли за границы то меняем положение ракеток иначе ничего не делаем
+            if (movement_left == 'a') {
+                y_racket_left = y_racket_left - 1; // двигаемся вверх
+            } else if (movement_left == 'z') {
+                y_racket_left = y_racket_left + 1; // двигаемся вниз
+            }
+        }
+
+        if (racket(y_racket_rigth, movement_rigth, 2) == 1) { // если мы не вышли за границы то меняем положение ракеток иначе ничего не делаем
+            if (movement_rigth == 'k') {
+                y_racket_rigth = y_racket_rigth - 1; // двигаемся вверх
+            } else if (movement_rigth == 'm') {
+                y_racket_rigth = y_racket_rigth + 1; // двигаемся вниз
+            }
+        }
+
+        // наверно здесь происходит все относительно мяча следовательно и подсчета очков
+
+        //  после команд снова выводим наше поле (но когда появится мяч то наверн несколько раз придется для быстроты выводить)
+        print_playing_field(0, y_racket_left, 79, y_racket_rigth, x_ball, y_ball, score_racket_left, score_racket_right);
+    }
 
     return 0;
 }
+
 
 // выводим игровое поле
 void print_playing_field(int x_racket1, int y_racket1, int x_racket2, int y_racket2, int x_ball, int y_ball,
@@ -31,7 +81,7 @@ void print_playing_field(int x_racket1, int y_racket1, int x_racket2, int y_rack
             for (int i = 0; i < 80; i++) {
                 printf("-");
             }
-        } else {
+        } else { // если не граница? то мы начинаем выводить остальные строки
             for (int i = 0; i < 80; i++) {
                 // мяч
                 if (x_ball == i && y_ball == y) {
@@ -52,7 +102,6 @@ void print_playing_field(int x_racket1, int y_racket1, int x_racket2, int y_rack
                     printf("|");
                 } else if (x_racket2 == i && (y_racket2 + 1) == y) {
                     printf("|");
-
                 } else if (x_racket2 == i && (y_racket2 + 2) == y) {
                     printf("|");
                 } else {
@@ -63,4 +112,20 @@ void print_playing_field(int x_racket1, int y_racket1, int x_racket2, int y_rack
         printf("\n");
         y++;
     }
+}
+
+
+//  проверка на выходы за границы ракеток чтоб чекнуть нужно ли менять их положение на поле
+int racket(int y_racket, char movement, int num) {
+    int ans = 0; // если не выходим за границы то возвращаем 1 следовательно будем потом менять положение ракеток
+    if (num == 1 && movement == 'a' && (y_racket - 1 != 0)) {
+        ans = 1;
+    } else if (num == 1 && movement == 'z' && (y_racket + 1 != 22)) {
+        ans = 1;
+    } else if (num == 2 && movement == 'k' && (y_racket - 1 != 0)) {
+        ans = 1;
+    } else if (num == 2 && movement == 'm' && (y_racket + 1 != 22)) {
+        ans = 1;
+    } 
+    return ans;
 }
