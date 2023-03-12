fd5770413a2082c7ed04a887d678277fc0eba701
lomysbro <lomysbro@student.21-school.ru>
2023-03-12 21:08:08 +0300

    s

--- src/pong.c
+++ src/pong.c
@@ -7,26 +7,21 @@ int racket(int y_racket, char movement, int num);
 int scan(char movement);
 
 int ball_in_racket_one(int x_ball, int y_ball, int y_racket_left, int BallPhase) {
-    if (x_ball == 1) {
-        if ((y_ball == y_racket_left) || (y_ball == y_racket_left - 1) || (y_ball == y_racket_left + 1)) {
-            if (BallPhase == -1) {
+    if (x_ball == 1)
+        if ((y_ball == y_racket_left) || (y_ball == y_racket_left - 1) || (y_ball == y_racket_left + 1))
+            if (BallPhase == -1)
                 return -2;
-            } else {
+            else
                 return 2;
-            }
-        }
-    }
     return BallPhase;
 }
 int ball_in_racket_two(int x_ball, int y_ball, int y_racket_rigth, int BallPhase) {
-    if (x_ball == 78) {
-        if ((y_ball == y_racket_rigth) || (y_ball == y_racket_rigth - 1) || (y_ball == y_racket_rigth + 1)) {
+    if (x_ball == 78)
+        if ((y_ball == y_racket_rigth) || (y_ball == y_racket_rigth - 1) || (y_ball == y_racket_rigth + 1))
             if (BallPhase == 2)
                 return 1;
             else
                 return -1;
-        }
-    }
     return BallPhase;
 }
 
@@ -52,7 +47,6 @@ int main(void) {
         else if (racket(y_racket_rigth, movement_rigth, 2) == 1 && movement_rigth == 'm')
             y_racket_rigth = y_racket_rigth + 1;  // двигаемся вниз
         BallPhase = ball_in_racket_one(x_ball, y_ball, y_racket_left, BallPhase);
-
         if (x_ball == 0) {
             if ((y_ball != y_racket_left) && (y_ball != y_racket_left - 1) && (y_ball != y_racket_left + 1)) {
                 score_racket_right++;
@@ -88,7 +82,6 @@ int main(void) {
                 x_ball = x_ball + 1;
                 break;
         }
-
         print_playing_field(0, y_racket_left, 79, y_racket_rigth, x_ball, y_ball, score_racket_left,
                             score_racket_right);
     }
@@ -98,27 +91,22 @@ int main(void) {
         printf("\033[1m\033[4m\033[32m2 win\033[0m\n");
     return 0;
 }
-
-// выводим игровое поле
 void print_playing_field(int x_racket1, int y_racket1, int x_racket2, int y_racket2, int x_ball, int y_ball,
                          int score1, int score2) {
     int y = 0;
-    for (int i = 0; i < 80; i++) {
-        if (i == 37) {
+    for (int i = 0; i < 80; i++)
+        if (i == 37)
             printf("%d", score1);
-        } else if (i == 40) {
+        else if (i == 40)
             printf("%d", score2);
-        } else
+        else
             printf(" ");
-    }
     printf("\n");
     while (y != 25) {
         if (y == 0 || y == 24) {
-            for (int i = 0; i < 80; i++) {
-                printf("-");
-            }
+            for (int i = 0; i < 80; i++) printf("-");
         } else {  // если не граница? то мы начинаем выводить остальные строки
-            for (int i = 0; i < 80; i++) {
+            for (int i = 0; i < 80; i++)
                 if (x_ball == i && y_ball == y)
                     printf("*");
                 else if (x_racket1 == i && y_racket1 == y)
@@ -135,7 +123,6 @@ void print_playing_field(int x_racket1, int y_racket1, int x_racket2, int y_rack
                     printf("|");
                 else
                     printf(" ");
-            }
         }
         printf("\n");
         y++;
@@ -144,22 +131,19 @@ void print_playing_field(int x_racket1, int y_racket1, int x_racket2, int y_rack
 
 int racket(int y_racket, char movement, int num) {
     int ans = 0;  // если не выходим за границы то возвращаем 1 следовательно будем
-    if (num == 1 && movement == 'a' && (y_racket - 1 != 0)) {
+    if (num == 1 && movement == 'a' && (y_racket - 1 != 0))
         ans = 1;
-    } else if (num == 1 && movement == 'z' && (y_racket + 1 != 22)) {
+    else if (num == 1 && movement == 'z' && (y_racket + 1 != 22))
         ans = 1;
-    } else if (num == 2 && movement == 'k' && (y_racket - 1 != 0)) {
+    else if (num == 2 && movement == 'k' && (y_racket - 1 != 0))
         ans = 1;
-    } else if (num == 2 && movement == 'm' && (y_racket + 1 != 22)) {
+    else if (num == 2 && movement == 'm' && (y_racket + 1 != 22))
         ans = 1;
-    }
     return ans;
 }
 
 int scan(char movement) {
     int ans = 0;
-    if (movement == 'z' || movement == 'a' || movement == ' ' || movement == 'k' || movement == 'm') {
-        ans = 1;
-    }
+    if (movement == 'z' || movement == 'a' || movement == ' ' || movement == 'k' || movement == 'm') ans = 1;
     return ans;
 }
